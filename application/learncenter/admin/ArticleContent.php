a429124feaa869b64173265693c5200fb00a7a5f
火线兔 <silverhawk@foxmail.com>
2022-10-31 17:45:30 +0800

    TuuzMac

--- application/learncenter/admin/ArticleContent.php
+++ application/learncenter/admin/ArticleContent.php
@@ -162,7 +162,7 @@ class ArticleContent extends Admin
                 ['text', 'title', '标题', ''],
                 ['text', 'content', '内容', ''],
                 ['file', 'url', '文件', ''],
-            ])
+            ]);
         return $data->setFormData($info) // 设置表单数据
         ->fetch();
     }
