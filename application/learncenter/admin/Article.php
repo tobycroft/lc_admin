616014eccb58ef3c133e4cd603765af1dee003f9
火线兔 <silverhawk@foxmail.com>
2022-11-14 15:07:12 +0800

    TuuzMac

--- application/learncenter/admin/Article.php
+++ application/learncenter/admin/Article.php
@@ -130,7 +130,24 @@ class Article extends Admin
                 $uids = UserInfoModel::where("tag_id", $data["tag_id"])->column("uid");
                 $users = UserModel::whereIn("id", $uids)->select();
                 foreach ($users as $user) {
-                    $Aoss->uniform_send($user["wx_id"], 'yS_JA3gE5-qg2fAqfTNr2mEc4-OB70AOX3afN1Oi_vQ', 'https://lc.familyeducation.org.cn/#/weeklyDuringPregnancy?id=' . $id, []);
+                    $Aoss->uniform_send($user["wx_id"], 'yS_JA3gE5-qg2fAqfTNr2mEc4-OB70AOX3afN1Oi_vQ', 'https://lc.familyeducation.org.cn/#/weeklyDuringPregnancy?id=' . $id, [
+                        'first' => [
+                            'value' => 'text1',
+                            'color' => '#173177',
+                        ],
+                        'keyword1' => [
+                            'value' => 'text1',
+                            'color' => '#173177',
+                        ],
+                        'keyword2' => [
+                            'value' => 'text1',
+                            'color' => '#173177',
+                        ],
+                        'remark' => [
+                            'value' => 'text1',
+                            'color' => '#173177',
+                        ],
+                    ]);
                 }
             }
 
