8e7124b0ebf9bf7f13e8382753832c898df0ada7
火线兔 <silverhawk@foxmail.com>
2022-12-05 10:46:25 +0800

    TuuzMac

--- application/learncenter/model/UserInfoModel.php
+++ application/learncenter/model/UserInfoModel.php
@@ -12,7 +12,7 @@ use think\Model;
 class UserInfoModel extends Model
 {
     // 设置当前模型对应的完整数据表名称
-    protected $table = 'lc_user';
+    protected $table = 'lc_user_info';
 
     // 设置当前模型对应的完整数据表名称
 
