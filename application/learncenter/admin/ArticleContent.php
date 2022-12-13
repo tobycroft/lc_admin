2ffffe907ca36a9c521f13c39d8b8387efa5d9df
火线兔 <silverhawk@foxmail.com>
2022-12-13 09:33:36 +0800

    TuuzMac

--- application/learncenter/admin/ArticleContent.php
+++ application/learncenter/admin/ArticleContent.php
@@ -48,7 +48,7 @@ class ArticleContent extends Admin
 
         $btn_access = [
             'title' => '用户地址',
-            'icon' => 'fa fa-fw fa-fw-jet',
+            'icon' => 'fa fa-fw fa-fighter-jet',
 //            'class' => 'btn btn-xs btn-default ajax-get',
             'href' => url('article/index', ['search_field' => 'id', 'keyword' => '__aid__'])
         ];
