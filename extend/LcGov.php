fed0c2193e4ab9bdb1842a7488109ed00990683c
火线兔 <silverhawk@foxmail.com>
2023-09-22 16:49:48 +0800

    TuuzMac

---
+++ extend/LcGov.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace LcGov;
+class LcGov
+{
+
+    private $url1 = "http://110.88.";
+    private $url2 = "153.177:901/Convergence/webservice/ConvergenceService?wsdl";
+
+    private function url()
+    {
+        return $this->url1 + $this->url2;
+    }
+
+
+}
