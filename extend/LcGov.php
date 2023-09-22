4c1771fbb2ef7fd10c382912f115231b6c14b697
火线兔 <silverhawk@foxmail.com>
2023-09-22 18:08:36 +0800

    TuuzMac

--- extend/LcGov.php
+++ extend/LcGov.php
@@ -10,6 +10,17 @@ class LcGov
     private $url2 = "153.177:901";
     private $path = "/Convergence/webservice/ConvergenceService?wsdl";
 
+    const 推送表 = "WEB612";
+    const 题库 = "WEB611";
+    const 文章内容 = "WEB610";
+    const 做题记录 = "WEB609";
+    const 用户信息 = "WEB608";
+    const 用户表 = "WEB607";
+    const 礼物表 = "WEB606";
+    const 礼物记录 = "WEB605";
+    const 文章 = "WEB604";
+    const 题目 = "WEB603";
+
     private function url()
     {
         return $this->url1 . $this->url2 . $this->path;
