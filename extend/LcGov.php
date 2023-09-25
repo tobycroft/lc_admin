d6ac485fba01c766ec96ad569c69f73f100acec9
火线兔 <silverhawk@foxmail.com>
2023-09-25 11:48:13 +0800

    TuuzMac

--- extend/LcGov.php
+++ extend/LcGov.php
@@ -83,7 +83,7 @@ class LcGov
      */
     public function toXml()
     {
-        $data = new ArrayToXml($this->xml_array, 'table', false, 320);
+        $data = new ArrayToXml($this->xml_array, 'table', false, "UTF-8");
         $data->setDomProperties(["formatOutput" => true]);
         return $data->toXml();
     }
