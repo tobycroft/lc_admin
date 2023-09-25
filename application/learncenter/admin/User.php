5c64438644e024493092a43a29508b9f533b377b
火线兔 <silverhawk@foxmail.com>
2023-09-25 09:27:15 +0800

    TuuzMac

--- application/learncenter/admin/User.php
+++ application/learncenter/admin/User.php
@@ -27,7 +27,7 @@ class User extends Admin
                 "id" => [
                     '_cdata' => "123",
                 ],
-//                "_attributes" => "add",
+                "_attributes" => ["type" => "add"],
             ],
 
         ];
