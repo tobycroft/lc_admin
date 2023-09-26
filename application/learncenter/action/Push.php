b9b31a154c36c60337f8e080ba9b328f91b49837
火线兔 <silverhawk@foxmail.com>
2023-09-26 15:12:41 +0800

    TuuzMac

--- application/learncenter/action/Push.php
+++ application/learncenter/action/Push.php
@@ -12,16 +12,16 @@ class Push
         $xml = new \LcGov();
         foreach ($datas as $data) {
             $xml->builder('add');
-            $xml->add_colums("id", $data["id"], "")
-                ->add_colums("quiz_id", $data["quiz_id"], "")
-                ->add_colums("tag", $data["tag"], "")
-                ->add_colums("title", $data["title"], "")
-                ->add_colums("content", $data["content"], "")
-                ->add_colums("correct", $data["correct"], "")
-                ->add_colums("a", $data["A"], "")
-                ->add_colums("b", $data["B"], "")
-                ->add_colums("c", $data["C"], "")
-                ->add_colums("d", $data["D"], "");
+            $xml->add_colums("id", $data["id"], "内容id")
+                ->add_colums("quiz_id", $data["quiz_id"], "文章id")
+                ->add_colums("tag", $data["tag"], "文章小标签")
+                ->add_colums("title", $data["title"], "题目")
+                ->add_colums("content", $data["content"], "内容")
+                ->add_colums("correct", $data["correct"], "正确选项")
+                ->add_colums("a", $data["A"], "选项a")
+                ->add_colums("b", $data["B"], "选项b")
+                ->add_colums("c", $data["C"], "选项c")
+                ->add_colums("d", $data["D"], "选项d");
 
         }
         $xml->toXml();
