99a898b72b52c1ee5b101637601cbc1fb71764d9
火线兔 <silverhawk@foxmail.com>
2023-09-26 16:31:29 +0800

    TuuzMac

--- extend/LcGov.php
+++ extend/LcGov.php
@@ -59,7 +59,7 @@ class LcGov
         var_dump($this->guid);
         $this->xml = $this->toXml();
 //        $this->client = new SoapClient($this->url());
-        $ret = $this->client->pushXml($this->guid, $catalogid, $this->xml);
+        $ret = $this->client->pushXmls($this->guid, $catalogid, $this->xml);
         var_dump($ret);
         return $ret->return;
     }
